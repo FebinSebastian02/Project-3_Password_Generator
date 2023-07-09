830c97e1a3525afffe0826a216501717996da8bf
Febin Sebastian <febin.vadakkan@gmail.com>
2023-07-09 11:44:35 +0530

    Commiting updated start and end lines

--- Main.py
+++ Main.py
@@ -3,11 +3,13 @@ from GeneratePassword import *
 
 def main():
     while True: #While loop is used to not close the program unless the user enters option 2
+        print("\n!!!Welcome User!!!")
         print("\nDo you need to create a random password? 1) Yes 2) No")
         choice = int(input("Enter an option..."))
         if choice == 1:
             genPassword()
         elif choice == 2:
+            print("\n!!!Bye, See you soon!!!")
             quit()
         else:
             print("\nInvalid option entered. Enter a valid option...")
